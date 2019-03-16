cb401ce61181ea37c575cfdcab7f3231b44d4f18
Srirupa <srirupadatta@pursuit.org>
2019-03-16 19:33:28 -0400

    created seed file with dummy data

---
+++ seed.sql
@@ -0,0 +1,60 @@
+DROP DATABASE IF EXISTS lowkey;
+CREATE DATABASE lowkey;
+
+\c lowkey;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  password VARCHAR NOT NULL,
+  token VARCHAR, 
+  type VARCHAR NOT NULL,
+  created TIMESTAMPTZ
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  author INT REFERENCES users(id) NOT NULL,
+  created TIMESTAMPTZ,
+  attachments VARCHAR NOT NULL,
+  content VARCHAR
+);
+
+CREATE TABLE connections (
+  id SERIAL PRIMARY KEY,
+  follower_user_id INT REFERENCES users(id) NOT NULL,
+  following_user_id INT REFERENCES users(id) NOT NULL,
+  status VARCHAR NOT NULL
+);
+
+CREATE TABLE likes (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) NOT NULL,
+  post_id INT REFERENCES posts(id) NOT NULL,
+  time TIMESTAMPTZ
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  author_id INT REFERENCES users(id) NOT NULL,
+  post_id INT REFERENCES posts(id) NOT NULL,
+  time TIMESTAMPTZ
+);
+
+INSERT INTO users (id, username, email, password, token, type, created) VALUES
+('1', 'John123', 'john@email.com', '123', 'xyz', 'private', 'today'), ('2', 'Michelle123', 'michelle@email.com', '123', 'xyz', 'public', 'yesterday');
+
+INSERT INTO posts (id, author, created, attachments, content) VALUES
+(01, '2', 'today', 'none', 'hello world'), (002, '1', 'today', 'photo', 'skghskfhskh'), (003, '1', 'yesterday', 'video', 'asksdgff');
+
+INSERT INTO connections (id, follower_user_id, following_user_id) VALUES
+('001', '2', '1'), ('002', '1', '2');
+
+INSERT INTO likes (id, user_id, post_id, time) VALUES
+('0001', '1', '002', 'today');
+
+INSERT INTO comments (id, author_id, post_id, time) VALUES
+('00001', '2', '01', 'today');
+
+
