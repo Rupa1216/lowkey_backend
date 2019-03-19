89a4ed0d7c48ee5f79bbad5489e3f98255d342c8
Srirupa <srirupadatta@pursuit.org>
2019-03-19 18:33:30 -0400

    separating db connection into its own module

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,7 +1,6 @@
 const pgp = require('pg-promise')({});
-const db = pgp('postgres://localhost/lowkey');
+const db = pgp(process.env.DATABASE_URL || 'postgres://localhost/lowkey');
 
 module.exports = {
     db,
 }
-
