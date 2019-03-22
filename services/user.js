e1799a47c2d89e12768e52c8f61d22853c66d2d3
Srirupa <srirupadatta@pursuit.org>
2019-03-22 18:35:51 -0400

    destructured import of db; changed parameter name to acct_type

--- services/user.js
+++ services/user.js
@@ -1,12 +1,26 @@
-const db = require('./dbConnect');
+const { db } = require('./dbConnect');
 const UserService = {};
 
-UserService.create = (username, email, password, token=null, type) => {
-    const sql = `
-    INSERT INTO users (username, email, password, token, type) VALUES ($[username], $[email], $[password], $[token], $[type]) 
+UserService.create = (username, email, password, token = null, acct_type) => {
+const sql = `
+    INSERT INTO users (username, email, password, token, type) 
+    VALUES ($[username], $[email], $[password], $[token], $[type]) 
     RETURNING id;
     `;
-    return db.one(sql, { username, email, password, token, type })
+    return db.one(sql, { username, email, password, token, acct_type })
+}
+
+UserService.create("Rupa", "me@me", "123", "xxyyzz", "private")
+
+UserService.update = (username, email, password, token, acct_type) => {
+    const sql = `
+    UPDATE users
+    SET
+    username=$[username], email=$[email], password=$[password], token=$[token], acct_type=$[acct_type]
+    WHERE
+    name=$[name]
+    `;
+    return db.one(sql, {username, email, password, token, acct_type})
 }
 
 module.exports = UserService;
