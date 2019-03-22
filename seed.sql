d78cbcb8a68e2471a361f38156f58f065aef2f6e
Srirupa <srirupadatta@pursuit.org>
2019-03-22 18:31:50 -0400

    removed password column in users table; changed some column titles; changed the value of the users' created_at column

--- seed.sql
+++ seed.sql
@@ -7,16 +7,15 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR UNIQUE NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
-    password VARCHAR NOT NULL,
     token VARCHAR, 
     acct_type VARCHAR NOT NULL,
-    created TIMESTAMPTZ
+    created_at INT DEFAULT CAST(EXTRACT(epoch FROM NOW()) AS INT) 
 );
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    author INT REFERENCES users(id) NOT NULL,
-    created TIMESTAMPTZ,
+    user_id INT REFERENCES users(id) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
     attachments VARCHAR NOT NULL,
     content VARCHAR
 );
@@ -32,29 +31,33 @@ CREATE TABLE likes (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id) NOT NULL,
     post_id INT REFERENCES posts(id) NOT NULL,
-    time TIMESTAMPTZ
+    created_at TIMESTAMPTZ
 );
 
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
-    author_id INT REFERENCES users(id) NOT NULL,
+    user_id INT REFERENCES users(id) NOT NULL,
     post_id INT REFERENCES posts(id) NOT NULL,
-    time TIMESTAMPTZ
+    created_at TIMESTAMPTZ
 );
 
-INSERT INTO users (id, username, email, password, token, acct_type, created) VALUES
-('1', 'John123', 'john@email.com', '123', 'xyz', 'private', 'today'), ('2', 'Michelle123', 'michelle@email.com', '123', 'xyz', 'public', 'yesterday');
+INSERT INTO users (username, email, token, acct_type) VALUES
+('John123', 'john@email.com', 'xyz', 'private'), 
+('Michelle123', 'michelle@email.com', 'xyz', 'public');
 
-INSERT INTO posts (id, author, created, attachments, content) VALUES
-('01', '2', 'today', 'none', 'hello world'), ('002', '1', 'today', 'photo', 'skghskfhskh'), ('003', '1', 'yesterday', 'video', 'asksdgff');
+INSERT INTO posts (user_id, created_at, attachments, content) VALUES
+('2', 'today', 'none', 'hello world'), 
+('1', 'today', 'photo', 'skghskfhskh'), 
+('1', 'yesterday', 'video', 'asksdgff');
 
-INSERT INTO connections (id, follower_user_id, following_user_id, status) VALUES
-('001', '2', '1', 'pending'), ('002', '1', '2', 'active');
+INSERT INTO connections (follower_user_id, following_user_id, status) VALUES
+('2', '1', 'pending'), 
+('1', '2', 'active');
 
-INSERT INTO likes (id, user_id, post_id, time) VALUES
-('0001', '1', '002', 'today');
+INSERT INTO likes (user_id, post_id, created_at) VALUES
+('1', '002', 'today');
 
-INSERT INTO comments (id, author_id, post_id, time) VALUES
-('00001', '2', '01', 'today');
+INSERT INTO comments (user_id, post_id, created_at) VALUES
+('2', '01', 'today');
 
 
