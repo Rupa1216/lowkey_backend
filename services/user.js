19dc9b668ecc9d6ecaeb9262f09f29209aa8b392
Srirupa <srirupadatta@pursuit.org>
2019-03-23 01:08:54 -0400

    added READ, DELETE, and allPublicUsers functions

--- services/user.js
+++ services/user.js
@@ -3,14 +3,23 @@ const UserService = {};
 
 UserService.create = (username, email, token = null, acct_type) => {
 const sql = `
-    INSERT INTO users (username, email, token, type) 
-    VALUES ($[username], $[email], $[token], $[type]) 
+    INSERT INTO users (username, email, token, acct_type) 
+    VALUES ($[username], $[email], $[token], $[acct_type]) 
     RETURNING id;
     `;
     return db.one(sql, { username, email, token, acct_type })
 }
 
-UserService.create("TK", "TK@gmail", "123lol", "nekot", "private")
+UserService.read = (username) => {
+    const sql = `
+    SELECT 
+    u.username, u.email, u.acct_type, u.created_at
+    FROM users u
+    WHERE
+    u.username = $[username]
+    `;
+    return db.one(sql, {username});
+}
 
 UserService.update = (username, email, token, acct_type) => {
     const sql = `
@@ -19,8 +28,26 @@ UserService.update = (username, email, token, acct_type) => {
     username=$[username], email=$[email], token=$[token], acct_type=$[acct_type]
     WHERE
     id=$[id]
-    `;
+    `; // should it be id or username in above line?
     return db.one(sql, {username, email, token, acct_type})
+}
+
+UserService.delete = (username) => {
+    const sql = `
+    DELETE FROM users u 
+    WHERE u.username=$[username]
+    `
+    return db.none(sql, {username});
+}
+
+UserService.allPublicUsers = (acct_type) => { // what parameter should I pass here?
+    const sql = `
+    SELECT
+    u.username, u.created_at
+    FROM users u
+    WHERE
+    u.$[acct_type] = 'public'
+    `;
 }
 
 module.exports = UserService;
