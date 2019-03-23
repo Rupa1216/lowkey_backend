f2677c01ed92c59432edac3c0c1e8e017c2e1739
Srirupa <srirupadatta@pursuit.org>
2019-03-23 02:04:19 -0400

    added POST route

--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,17 @@ const express = require('express');
 const userRouter = express.Router();
 const UserService = require('../services/user');
 
+// POST - CREATE 
+userRouter.post('/', (req, res, next) => {
+    const {username, email} = req.body;
+
+    UserService.create(username, email)
+    .then(data => {
+        res.json({success: `Created user named ${username} with generated ID: ${data.id}`});
+    })
+    .catch(err => {
+        next(err);
+    })
+});
 
 module.exports = userRouter;
