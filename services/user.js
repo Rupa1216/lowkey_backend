ce338a0b89fd5119d52492247d039eed0544e99f
Srirupa <srirupadatta@pursuit.org>
2019-03-23 01:06:44 -0400

    removed password parameter from CREATE and UPDATE functions, changed WHERE constraint in update function

--- services/user.js
+++ services/user.js
@@ -1,26 +1,26 @@
 const { db } = require('./dbConnect');
 const UserService = {};
 
-UserService.create = (username, email, password, token = null, acct_type) => {
+UserService.create = (username, email, token = null, acct_type) => {
 const sql = `
-    INSERT INTO users (username, email, password, token, type) 
-    VALUES ($[username], $[email], $[password], $[token], $[type]) 
+    INSERT INTO users (username, email, token, type) 
+    VALUES ($[username], $[email], $[token], $[type]) 
     RETURNING id;
     `;
-    return db.one(sql, { username, email, password, token, acct_type })
+    return db.one(sql, { username, email, token, acct_type })
 }
 
-UserService.create("Rupa", "me@me", "123", "xxyyzz", "private")
+UserService.create("TK", "TK@gmail", "123lol", "nekot", "private")
 
-UserService.update = (username, email, password, token, acct_type) => {
+UserService.update = (username, email, token, acct_type) => {
     const sql = `
     UPDATE users
     SET
-    username=$[username], email=$[email], password=$[password], token=$[token], acct_type=$[acct_type]
+    username=$[username], email=$[email], token=$[token], acct_type=$[acct_type]
     WHERE
-    name=$[name]
+    id=$[id]
     `;
-    return db.one(sql, {username, email, password, token, acct_type})
+    return db.one(sql, {username, email, token, acct_type})
 }
 
 module.exports = UserService;
