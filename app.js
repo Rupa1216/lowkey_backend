e3a26f61fb5fddafa1d05d211db0bc8ba22e9421
Srirupa <srirupadatta@pursuit.org>
2019-03-23 01:34:13 -0400

    imported userRouter and added it under middleware

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express');
 const bodyParser = require('body-parser');
-
+const express = require('express');
 const app = express();
+
+const userRouter = require('./routes/user');
+
 
 // MIDDLEWARE NEEDED
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+// ROUTES
+app.use('/users', userRouter);
+
 
 module.exports = { app, }
