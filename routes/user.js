585416f83f722b7f11defbeea87c252903f7d2d5
Srirupa <srirupadatta@pursuit.org>
2019-03-24 11:45:51 -0400

    added PUT and DELETE routes

--- routes/user.js
+++ routes/user.js
@@ -18,14 +18,42 @@ userRouter.post('/', (req, res, next) => {
 // GET - READ 
 userRouter.get('/:username/', (req, res, next) => {
     const {username} = req.params;
-  
+
     UserService.read(username)
-      .then(data => {
+    .then(data => {
         res.json(data);
-      })
-      .catch(err => {
+    })
+    .catch(err => {
         next(err);
-      })
-  });
+    })
+});
+
+// PUT - UPDATE EMAIL
+userRouter.put('/:username', (req, res, next) => {
+    const { username } = req.params;
+    const { email} = req.body;
+// would token be updated through here?
+
+UserService.update(username, email)
+    .then(data => {
+    res.json({success: `Updated user named ${username} with email: ${email}`});
+    })
+    .catch(err => {
+    next(err);
+    })
+});
+
+// DELETE - DELETE
+userRouter.delete('/:username', (req, res, next) => {
+const {username} = req.params;
+
+UserService.delete(username)
+    .then(data => {
+    res.json({success: `Deleted user named ${username}`});
+    })
+    .catch(err => {
+    next(err);
+    })
+});
 
 module.exports = userRouter;
