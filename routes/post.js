c2086979e100fb0616826cf408b3f5085e0439a0
Srirupa <srirupadatta@pursuit.org>
2019-03-24 16:39:39 -0400

    added READ, PUT, and DELETE routes

--- routes/post.js
+++ routes/post.js
@@ -4,15 +4,55 @@ const PostService = require('../services/post');
 
 // POST - CREATE 
 postRouter.post('/', (req, res, next) => {
-    const {user_id, created_at, content, attachments} = req.body;
+    const {user_id, created_at, content} = req.body;
 
-    PostService.create(user_id, created_at, content, attachments)
+    PostService.create(user_id, created_at, content)
     .then(data => {
-        res.json({success: `Created post for ${user_id} with generated id: ${data.id}`});
+        res.json({success: `Created post for user ${user_id} with generated id: ${data.id}`});
     })
     .catch(err => {
         next(err);
     })
 });
+
+// GET - READ 
+postRouter.get('/:id/', (req, res, next) => {
+    const {id} = req.params;
+
+    PostService.read(id)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        next(err);
+    })
+});
+
+// PUT - UPDATE
+postRouter.put('/:id', (req, res, next) => {
+    const { id } = req.params;
+    const { content } = req.body;
+
+PostService.update(content, id)
+    .then(data => {
+    res.json({success: `Updated post number ${id} with content: ${content}`});
+    })
+    .catch(err => {
+    next(err);
+    })
+});
+
+// DELETE - DELETE
+postRouter.delete('/:id', (req, res, next) => {
+    const {id} = req.params;
+    
+    PostService.delete(id)
+        .then(data => {
+        res.json({success: `Deleted post number ${id}`});
+        })
+        .catch(err => {
+        next(err);
+        })
+    });
 
 module.exports = postRouter;
