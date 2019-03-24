2d8c87340f20085b481acbbda519de8aa181b64e
Srirupa <srirupadatta@pursuit.org>
2019-03-24 12:14:56 -0400

    added POST route

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,17 @@ const express = require('express');
 const postRouter = express.Router();
 const PostService = require('../services/post');
 
+// POST - CREATE 
+postRouter.post('/', (req, res, next) => {
+    const {user_id, created_at, content, attachments} = req.body;
 
+    PostService.create(user_id, created_at, content, attachments)
+    .then(data => {
+        res.json({success: `Created post for ${user_id} with generated id: ${data.id}`});
+    })
+    .catch(err => {
+        next(err);
+    })
+});
 
 module.exports = postRouter;
