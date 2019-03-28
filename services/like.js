8d65813d98a15eae90995ede47dcaae8e30cbbcb
Srirupa <srirupadatta@pursuit.org>
2019-03-28 19:58:32 -0400

    added create function

--- services/like.js
+++ services/like.js
@@ -1,6 +1,13 @@
 const { db } = require('./dbConnect');
 const LikeService = {};
 
-
+LikeService.create = (user_id, post_id) => {
+    const sql = `
+        INSERT INTO likes (user_id, post_id) 
+        VALUES ($[user_id], $[post_id]) 
+        RETURNING id;
+        `;
+        return db.one(sql, { user_id, post_id })
+    }
 
 module.exports = LikeService;
