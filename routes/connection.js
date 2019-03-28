9247c61d732577c7c7047085a0dbde233fe9a4c7
Srirupa <srirupadatta@pursuit.org>
2019-03-27 21:56:40 -0400

    added POST route

--- routes/connection.js
+++ routes/connection.js
@@ -2,6 +2,19 @@ const express = require('express');
 const connectionRouter = express.Router();
 const ConnectionService = require('../services/connection');
 
+// POST - CREATE 
+connectionRouter.post('/', (req, res, next) => {
+    const { follower_id, following_id, status } = req.body;
+
+    ConnectionService.create(follower_id, following_id, status)
+    .then(data => {
+        res.json({success: `Created connection between ${follower_id} and ${following_id} with generated ID: ${data.id}`});
+    })
+    .catch(err => {
+        next(err);
+    })
+});
+
 
 
 module.exports = connectionRouter;
