b8344368a099b3af9d76e6d58fd8ac10dc25eeee
Srirupa <srirupadatta@pursuit.org>
2019-03-28 20:59:27 -0400

    added POST route

--- routes/like.js
+++ routes/like.js
@@ -2,4 +2,17 @@ const express = require('express');
 const likeRouter = express.Router();
 const LikeService = require('../services/connection');
 
+// POST - CREATE 
+likeRouter.post('/', (req, res, next) => {
+    const { user_id, post_id } = req.body;
+
+    LikeService.create(user_id, post_id)
+    .then(data => {
+        res.json({success: `User #${user_id} liked post #${post_id} with generated ID: ${data.id}`});
+    })
+    .catch(err => {
+        next(err);
+    })
+});
+
 module.exports = likeRouter;
