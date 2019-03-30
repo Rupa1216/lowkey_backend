f86a49ead85d74925de88e8e9f4ca27f161e4c37
Srirupa <43832316+Rupa1216@users.noreply.github.com>
2019-03-30 17:33:39 -0400

    Firebase (#4)
    * connected firebase
    
    * ignoring firebase key
    
    * added firebase key
    
    * imported and implemented cors and firebase admin SDK

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const app = express();
+const admin = require('./firebase');
 
 const userRouter = require('./routes/user');
 const postRouter = require('./routes/post');
@@ -10,10 +12,24 @@ const likeRouter = require('./routes/like');
 
 
 // MIDDLEWARE NEEDED
+app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+const checkFirebaseToken = (req, res, next) => {
+    const { token } = req.body;
+  
+    admin.auth().verifyIdToken(token)
+      .then(function(decodedToken) {
+        var uid = decodedToken.uid;
+        next();
+      }).catch(function(error) {
+        // Handle error
+        res.json('Error!')
+      });
+  }
 
 // ROUTES
 app.use('/users', userRouter);
@@ -30,5 +46,14 @@ app.use((err, req, res, next) => {
 app.get('/', (req, res) => {
     res.json({'test': true})
 })
+
+app.post('/unprotected', (req, res) => {
+    const { email, id } = req.body;
+    res.json(`Welcome! You are user ${email} with ${id}`);
+  })
+  
+  app.post('/protected', checkFirebaseToken, (req, res) => {
+    res.json(`This is some sensitive data`);
+  })
 
 module.exports = { app, }
