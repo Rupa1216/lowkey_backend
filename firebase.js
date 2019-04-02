20376d7bcfa1407cd20efd43cba7a2da2cbe1221
Srirupa <srirupadatta@pursuit.org>
2019-04-01 23:10:15 -0400

    added new firebase key

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase_key.json");
+var serviceAccount = require(process.env.FIREBASE_KEY || "./firebase_key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
