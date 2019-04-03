9a3c3b45dd014bcb7cb2192921befc04868f0fa5
Srirupa <43832316+Rupa1216@users.noreply.github.com>
2019-04-02 21:38:32 -0400

    Firebase (#6)
    * connected firebase
    
    * ignoring firebase key
    
    * added firebase key
    
    * imported and implemented cors and firebase admin SDK
    
    * added new firebase environment
    
    * parsing firebase_key

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@ var admin = require("firebase-admin");
 
 var serviceAccount;
 
-if(process.env.FIREBASE_KEY) {
-    serviceAccount = process.env.FIREBASE_KEY;
+if (process.env.FIREBASE_KEY) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 }
 else {
     serviceAccount = require("./firebase_key.json");
 }
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://lowkey-frontend.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://lowkey-frontend.firebaseio.com"
 });
